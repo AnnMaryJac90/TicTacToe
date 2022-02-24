4ecce386fe3a29a3a3285ca90e870675a33f3e65
Ann Mary <annmaryjac90@gmail.com>
2022-02-24 11:05:00 -0800

    Both human turn and machine turns happening

--- TicTacToe/ContentView.swift
+++ TicTacToe/ContentView.swift
@@ -7,11 +7,17 @@
 
 import SwiftUI
 
-let coloums : [GridItem] = [GridItem(.flexible()),
-                            GridItem(.flexible()),
-                            GridItem(.flexible())]
+
 
 struct ContentView: View {
+    let coloums : [GridItem] = [GridItem(.flexible()),
+                                GridItem(.flexible()),
+                                GridItem(.flexible())]
+    
+    @SwiftUI.State private var moves : [Move?] = Array(repeating: nil, count: 9)
+    
+    @SwiftUI.State private var isHuman = true
+    
     var body: some View {
         GeometryReader { geometry in
             VStack{
@@ -22,7 +28,22 @@ struct ContentView: View {
                         Circle()
                             .foregroundColor(.blue)
                             .frame(width: geometry.size.width/3-10, height: geometry.size.width/3-10, alignment: .center)
+                        Image(systemName: moves[i]?.indicator ?? "")
+                            .resizable()
+                            .frame(width: 40, height: 40, alignment: .center)
                             
+                    }
+                   
+                    .onTapGesture {
+                        if isCircleOccupied(in: moves, at: i){
+                            return
+                        }
+                        moves[i] = Move(player: .human, index: i)
+                        DispatchQueue.main.asyncAfter(deadline: .now()+0.5){
+                            let machinePosition = determineMachineMovePosition(in: moves)
+                            moves[machinePosition] = Move(player: .machine, index: machinePosition)
+                        }
+                       
                     }
                 }
 
@@ -33,10 +54,49 @@ struct ContentView: View {
             }.padding()
         }
     }
+    
+    func isCircleOccupied(in moves:[Move?],at index: Int)->Bool{
+        return moves.contains(where: {$0?.index == index})
+    }
+    
+    
+    func determineMachineMovePosition(in moves: [Move?])-> Int{
+        var position = Int.random(in: 0..<9)
+        while( isCircleOccupied(in: moves, at: position)){
+           position = Int.random(in: 0..<9)
+        }
+        return position
+    }
+    
 }
+
+
+
+
+
+
+enum Player {
+    case human, machine
+}
+
+struct Move{
+    var player : Player
+    var index : Int
+    var indicator : String {
+        return player == .human ? "xmark" : "circle"
+    }
+}
+
+
+
+
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
+
+
